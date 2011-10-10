866e661c6941a20adc0069603f3ad3f7c1baf749
superp <superp1987@gmail.com>
2011-10-10 11:58:58 +0300

    fix specs tweetracker

--- spec/providers/tweetracker_spec.rb
+++ spec/providers/tweetracker_spec.rb
@@ -5,8 +5,13 @@ describe ShareChecker::Providers::Tweetracker do
     @provider = ShareChecker.provider("tweetracker", "http://www.google.com.ua/", :group_id => 'test')
   end
   
-  it "should parse response from tweetracker" do
+  it "should parse json response from tweetracker" do
     response = "{\"tweets\":30,\"id\":\"4e281dcec546612414000004\",\"users\":26,\"query\":\"http://www.mcdonaldseurokids2012.com.ua/challenges/1/works/140\"}"
+    @provider.parse_json(response)["users"].should == 26
+  end
+  
+  it "should parse xml response from tweetracker" do
+    response = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<track-item>\n  <query>http://www.mcdonaldseurokids2012.com.ua/works/474</query>\n  <tweets type=\"integer\">1</tweets>\n  <users type=\"integer\">26</users>\n  <id>4e8f06abc546611f21000177</id>\n</track-item>\n"
     @provider.parse(response).should == 26
   end
 end
